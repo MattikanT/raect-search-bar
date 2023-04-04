aae5b67ae6cfceb9eb50377fccf05a4289b920fe
Mattikan Tongsai <117744905+MattikanT@users.noreply.github.com>
2023-04-04 08:39:51 +0300

    Initial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css";
+
+export const SearchBar = () => {
+  const [input, setInput] = useState("");
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((Response) => Response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        type="text"
+        placeholder="Type to search....."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
